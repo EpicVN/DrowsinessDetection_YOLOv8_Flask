fb0f37bcc30790d213a1313db726d1fc5c2d4ff4
EpicVN <ledangkhoahi3@gmail.com>
2024-01-14 12:03:49 +0700

    DrowsinessDetection_YOLOv8_Flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, Response, session
+from flask import Flask, render_template, Response, session, url_for, redirect, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user, logout_user
+from flask_bcrypt import Bcrypt
 
 #FlaskForm--> it is required to receive input from the user
-# Whether uploading a video file  to our object detection model
-
 from flask_wtf import FlaskForm
-
-
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, StringField, PasswordField
 from werkzeug.utils import secure_filename
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, ValidationError
 import os
 
 
@@ -18,11 +17,49 @@ import cv2
 # YOLO_Video is the python file which contains the code for our object detection model
 #Video Detection is the Function which performs Object Detection on Input Video
 from YOLO_Video import video_detection
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ayush'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+app.config['SECRET_KEY'] = 'epicvn'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+#User
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError('That username already exists. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
 
 #Use FlaskForm to get input video file  from user
 class UploadFileForm(FlaskForm):
@@ -50,19 +87,51 @@ def generate_frames_web(path_x):
         yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
 
+
 @app.route('/', methods=['GET','POST'])
+def welcome():
+    session.clear()
+    return render_template('welcome.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/home', methods=['GET','POST'])
 def home():
-    session.clear()
-    return render_template('home.html')
-# Rendering the Webcam Rage
-#Now lets make a Webcam page for the application
-#Use 'app.route()' method, to render the Webcam page at "/webcam"
-@app.route("/Webcam", methods=['GET','POST'])
+    return render_template('dashboard.html')
 
+@app.route('/dataset', methods=['GET','POST'])
+def dataset():
+    return render_template('dataset.html')
+
+@app.route("/Webcam", methods=['GET','POST'])
 def webcam():
-    session.clear()
     return render_template('webcam.html')
+
 @app.route('/Video_Image', methods=['GET','POST'])
 def front():
     # Upload File Form: Create an instance for the Upload File Form
@@ -70,22 +139,23 @@ def front():
     if form.validate_on_submit():
         # Our uploaded video file path is saved here
         file = form.file.data
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
-                               secure_filename(file.filename)))  # Then save the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))  # Then save the file
         # Use session storage to save video file path
-        session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
-                                             secure_filename(file.filename))
+        session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
     return render_template('video_image_detection.html', form=form)
+
+
 @app.route('/video')
 def video():
     #return Response(generate_frames(path_x='static/files/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
     return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 # To display the Output Video on Webcam page
 @app.route('/webapp')
 def webapp():
-    #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
     return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
